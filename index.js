328f8edfc7fc2c0312fd090e75db00890ac2f1ba
Cheshan782 <88943662+Cheshan782@users.noreply.github.com>
2021-10-24 00:31:26 -0400

    1

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 function updateClock(){
     var now = new Date();
-    var day = now.getDay();
-    var mo = now.getMonth();
-    var num = now.getDate();
-    var yr = now.getFullYear();
-    var hr = now.getHour();
-    var min = now.getMinutes();
-    var sec = now.getSeconds();
-    var pe = "AM";
+    var dname = now.getDay(),
+        mo = now.getMonth(),
+        dnum = now.getDate(),
+        yr = now.getFullYear(),
+        hou = now.getHours(),
+        min = now.getMinutes(),
+        sec = now.getSeconds(),
+        pe = "AM";
+        
+        if(hou >= 12){
+          pe = "PM";
+        }
+        if(hou == 0){
+          hou = 12;
+        }
+        if(hou > 12){
+          hou = hou - 12;
+        }
 
-    var months = ["Janurary", "Feburary", "March", "April", "May", "June", "July", "August", "Spetember", "October", "November", "December", ]
-    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    var ids = ["Day", "Month", "Num", "Year", "Hours", "Min", "Sec", "PE", ] 
-    var values = [day, mo, num, yr, hr, min, sec, pe]
+        Number.prototype.pad = function(digits){
+          for(var n = this.toString(); n.length < digits; n = 0 + n);
+          return n;
+        }
 
-    for (var i = 0; i < ids.length; i++)
-        document(getElementById(ids[1]).firstChild.nodeValue = values[i]);
-    
+        var months = ["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+        var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+        var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+        for(var i = 0; i < ids.length; i++)
+        document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+  }
 
-
-}
-
-function initClock(){
+  function initClock(){
     updateClock();
-    window.setInterval("updateClock(), 1");
-}
+    window.setInterval("updateClock()", 1);
+  }
 
